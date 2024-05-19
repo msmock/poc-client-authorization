8428f6012e761563c90a1c720d428267e6cba017
Martin Smock <martin_smock@me.com>
2024-05-19 22:11:26 +0200

    access token attributes

--- poc-resource-server/src/main/java/org/nahsi/service/poc/controllers/ProtectedResourceController.java
+++ poc-resource-server/src/main/java/org/nahsi/service/poc/controllers/ProtectedResourceController.java
@@ -2,9 +2,11 @@ package org.nahsi.service.poc.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @EnableMethodSecurity
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProtectedResourceController {
 
     @GetMapping("/protected")
-    public String demo(Authentication a) {
+    public String demo(BearerTokenAuthentication authentication) {
 
-        System.out.println(a);
+        // TODO evaluate token attributes
+
+        Map<String, Object> tokenAttributes = authentication.getTokenAttributes();
+        System.out.println(">>> subject : "+ tokenAttributes.get("sub") );
+        System.out.println(">>> audience : "+ tokenAttributes.get("aud") );
 
         return "Protected endpoint, for scope MIX only ...";
     }
